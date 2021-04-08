b393d97f7d344f734f94cb97b0e546c2ef3a096c
YeonJae Nam <lisanamyj99@gmail.com>
2021-04-08 14:21:48 +0900

    수정

--- node1/소스.cpp
+++ node1/소스.cpp
@@ -10,10 +10,10 @@ public:    //멤버함수를 외부에서 사용할 수 있도록 함
 
 Node* Head = NULL;//헤드 노드는 전역변수, 첫번째 노드를 가리킬 수 있음
 
-void insert_node(Node *new_node,Node *pre)    {	//강의의 A,B,C방식대로
+void insert_node(Node *pre, Node*new_node)    {	//강의의 A,B,C방식대로
 	//A방식
 	if (Head == NULL) {	// 공백리스트인 경우
-		// new_node->link = NULL; 
+		 new_node->link = Head; 
 		Head = new_node;
 	}
 	else if (pre == NULL) { 	// pre가 NULL이면 첫번째 노드로 삽입
@@ -28,7 +28,11 @@ void insert_node(Node *new_node,Node *pre)    {	//강의의 A,B,C방식대로
 
 void print_list()	{
 	for (Node* ptr = Head; ptr != NULL; ptr = ptr->link)
-		cout << ptr->data << endl;
+		if (ptr->link == NULL)
+		
+			cout << ptr->data << endl;
+		else
+			cout << ptr->data << " --> ";
 }
 
 void main()    {
@@ -37,6 +41,7 @@ void main()    {
 	{
 			//새로운 8개의 데이터를 차례대로 입력받음
 		int i_data;
+		cout << "새로운 값을 입력하시오  (#"<<i+1<<"):  ";
 		cin >> i_data;
 			//새로운 노드를 생성, 내용을 수정
 		Node* new_node = new Node;
@@ -47,4 +52,6 @@ void main()    {
 		insert_node(NULL,new_node);
 
 	}
+	//확인용으로 출력
+	print_list();
 }
